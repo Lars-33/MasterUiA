622649053e441f3692d9f245859cf35b6fe8327b
Lars-33 <larsmuggerud@proton.me>
2023-02-10 16:18:50 +0100

    dio

--- tb_ws/src/tb3_cpp/launch/rviz_launch.py
+++ tb_ws/src/tb3_cpp/launch/rviz_launch.py
@@ -1,15 +1,12 @@
 import os
 
-from ament_index_python.packages import get_package_share_directory
-
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
-import launch
 
+os.environ["TURTLEBOT3_MODEL"] = "waffle"
 
-base_path = os.path.realpath(get_package_share_directory('tb3_cpp'))
-rviz_path=base_path+'/params/test.rviz'
+print('hei')
 
 def generate_launch_description():
 
@@ -17,14 +14,15 @@ def generate_launch_description():
    return LaunchDescription([
 
         Node(
-            package='rviz2',
-            executable='rviz2',
-            name='rviz2',
-            output='screen',
-            arguments=['-d',str(rviz_path)]
-            #arguments=['-d', str(.rviz2/lars.rviz)]
-        )
+            package='turtlesim',
+            executable='turtlesim_node'
+        ),
 
+        Node( 
+            package='turtlebot3_teleop',
+            executable='teleop_keyboard',
+            output='screen'  
+        )
 
    ])
 
