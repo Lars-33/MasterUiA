8c0d2de30d3d94a8347d50ea4a60bb02a57dee06
Lars-33 <larsmuggerud@proton.me>
2023-02-16 13:40:56 +0100

    rviz rqt launch file added

---
+++ tb_ws/src/tb3_cpp/launch/rviz_launch.py
@@ -0,0 +1,38 @@
+import os
+
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from ament_index_python import get_package_share_directory
+from launch_ros.actions import Node
+
+
+
+
+os.environ["TURTLEBOT3_MODEL"] = "waffle"
+
+print('hei')
+
+def generate_launch_description():
+
+   return LaunchDescription([
+
+
+        Node(
+            package='rqt_graph',
+            executable='rqt_graph'
+        ),
+
+        # ros2 launch nav2_bringup rviz_launch.py 
+        IncludeLaunchDescription(
+            
+            PythonLaunchDescriptionSource(
+                os.path.join(
+                    get_package_share_directory('nav2_bringup'),
+                    'launch/rviz_launch.py'
+                )
+            ),
+        )
+
+   ])
+
