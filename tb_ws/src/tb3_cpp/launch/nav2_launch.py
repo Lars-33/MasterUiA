efbf6a68548925e35f4cdf4bc1bc0a2de7e93c07
Lars-33 <larsmuggerud@proton.me>
2023-02-16 09:10:23 +0100

    nav2 launch file

---
+++ tb_ws/src/tb3_cpp/launch/nav2_launch.py
@@ -0,0 +1,59 @@
+import os
+
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from ament_index_python import get_package_share_directory
+
+
+
+os.environ["TURTLEBOT3_MODEL"] = "waffle"
+
+print('hei')
+
+def generate_launch_description():
+
+
+   return LaunchDescription([
+
+        #  ros2 launch slam_toolbox online_async_launch.py 
+        IncludeLaunchDescription(
+            
+            PythonLaunchDescriptionSource(
+                os.path.join(
+                    get_package_share_directory('slam_toolbox'),
+                    'launch/online_async_launch.py'
+                )
+            ),
+        ),
+
+
+        # ros2 launch nav2_bringup localization_launch.py 
+        IncludeLaunchDescription(
+            
+            PythonLaunchDescriptionSource(
+                os.path.join(
+                    get_package_share_directory('nav2_bringup'),
+                    'launch/localization_launch.py'
+                )
+            ),
+                    
+            launch_arguments={
+                'map':'/home/lars/MasterUiA/tb_ws/map/Masterlabben.yaml'
+                    
+                }.items()
+        )
+
+        # ros2 launch nav2_bringup rviz_launch.py 
+        #IncludeLaunchDescription(
+        #    
+        #    PythonLaunchDescriptionSource(
+        #        os.path.join(
+        #            get_package_share_directory('nav2_bringup'),
+        #            'launch/rviz_launch.py'
+        #        )
+        #    ),
+        #)
+
+   ])
+
