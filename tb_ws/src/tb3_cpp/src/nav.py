b71d422987e3ac40780d9e58f91ce14f9d921c04
Lars-33 <larsmuggerud@proton.me>
2023-03-09 09:17:11 +0100

    addded python scripts

---
+++ tb_ws/src/tb3_cpp/src/nav.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+def main():
+    print('Hi from nav2.')
+
+if __name__ == '__main__':
+    main()
---
+++ tb_ws/src/tb3_cpp/src/node.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+def main():
+    print('Hi from python in a cpp pkg.')
+
+if __name__ == '__main__':
+    main()
