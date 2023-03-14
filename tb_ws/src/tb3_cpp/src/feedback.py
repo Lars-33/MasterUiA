f3c63353088ee7f22ddc96ecd513ba042420aa2c
Lars-33 <larsmuggerud@proton.me>
2023-03-14 12:49:57 +0100

    feedback.py

---
+++ tb_ws/src/tb3_cpp/src/feedback.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+import rclpy 
+from nav2_simple_commander.robot_navigator  import BasicNavigator
+
+
+def main(): 
+    rclpy.init()
+    nav = BasicNavigator()
+
+    # --- Wait for Nav2 
+    nav.waitUntilNav2Active()
+
+
+    print(nav)
+    print( nav.getFeedback() , '\n')
+    while not nav.isTaskComplete() or True: 
+        print(nav, '\n')
+        print(nav.getFeedback() , '\n----------------------------\n\n')
+
+    rclpy.shutdown()
+
+if __name__ == '__main__' : 
+    main()
