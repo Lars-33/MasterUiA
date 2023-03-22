1693ac2aacdd8f63eb80c0466dbe8238f3725ec9
Lars-33 <larsmuggerud@proton.me>
2023-03-22 12:34:48 +0100

    PushRosNamespace added to tb launch

--- tb_ws/src/tb3_cpp/launch/nav2_gazebo_launch.py
+++ tb_ws/src/tb3_cpp/launch/nav2_gazebo_launch.py
@@ -6,7 +6,8 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 from ament_index_python import get_package_share_directory
 from launch_ros.actions import Node
 from launch_ros.actions import SetRemap
-
+from launch.actions import GroupAction
+from launch_ros.actions import PushRosNamespace
 
 os.environ["TURTLEBOT3_MODEL"] = "waffle"
 
@@ -14,23 +15,29 @@ print('hei')
 
 def generate_launch_description():
 
-   return LaunchDescription([
-        #SetRemap(src='/odom',dst='follower/odom'),
-        #SetRemap(src='/cmd_vel',dst='follower/cmd_vel'),
-        
-        # ros2 launch nav2_bringup rviz_launch.py 
-        IncludeLaunchDescription(
-            
-            PythonLaunchDescriptionSource(
-                os.path.join(
-                    get_package_share_directory('turtlebot3_gazebo'),
-                    'launch/spawn_turtlebot3.launch.py'
-                )
-            ),
-            launch_arguments={
-                'x_pose':'-2.0',
-                'y_pose':'0.5' 
-            }.items()
-        )
+    return LaunchDescription([
 
+        GroupAction(
+            actions=[
+                #SetRemap(src='/odom',dst='follower/odom'),
+                #SetRemap(src='/cmd_vel',dst='follower/cmd_vel'),
+                #SetRemap(src='/scan',dst='follower/scan'),
+                PushRosNamespace('tb'),
+                
+                # ros2 launch turtlebot3_gazebo turtlebot3_world.launch.py 
+                IncludeLaunchDescription(
+                    
+                    PythonLaunchDescriptionSource(
+                        os.path.join(
+                            get_package_share_directory('turtlebot3_gazebo'),
+                            'launch/turtlebot3_world.launch.py'
+                        )
+                    ),
+                    launch_arguments={
+                        'x_pose':'-2.0',
+                        'y_pose':'0.5' 
+                    }.items()
+                ),
+            ]
+        )
    ])
