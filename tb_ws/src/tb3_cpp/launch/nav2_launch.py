8aa1c2bc9047fab6fe0594a9264f81634718d731
Lars-33 <larsmuggerud@proton.me>
2023-03-27 19:37:32 +0200

    aa

--- tb_ws/src/tb3_cpp/launch/nav2_launch.py
+++ tb_ws/src/tb3_cpp/launch/nav2_launch.py
@@ -37,7 +37,9 @@ def generate_launch_description():
                 ),    
                 launch_arguments={
                     #'map':'map/Masterlabben.yaml',
-                    'params_file':'src/tb3_cpp/params/nav2_param.yaml'
+                    'params_file':'src/tb3_cpp/params/nav2_param.yaml',
+                    'namespace':'tb',
+                    'use_namespace':'true'
                         
                 }.items()
             )
