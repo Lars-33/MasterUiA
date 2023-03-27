5e89da508ba47d8decf1368684f560ab12d963f4
Lars-33 <larsmuggerud@proton.me>
2023-03-27 12:12:32 +0200

    aa

--- tb_ws/src/uia_husky_0776/uia_master_husky/husky_control/launch/teleop_joy_launch.py
+++ tb_ws/src/uia_husky_0776/uia_master_husky/husky_control/launch/teleop_joy_launch.py
@@ -2,6 +2,10 @@ from launch import LaunchContext, LaunchDescription
 from launch.substitutions import EnvironmentVariable, PathJoinSubstitution
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
+from launch.actions import DeclareLaunchArgument
+from launch_ros.actions import Node
+from launch.substitutions import Command, FindExecutable, PathJoinSubstitution, LaunchConfiguration
+from launch_ros.substitutions import FindPackageShare
 
 ARGUMENTS = [
 
