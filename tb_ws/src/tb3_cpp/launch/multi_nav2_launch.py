1e6363a713e00b58e56d6d2297932624738ccd53
Lars-33 <larsmuggerud@proton.me>
2023-03-28 21:08:28 +0200

    aa

---
+++ tb_ws/src/tb3_cpp/launch/multi_nav2_launch.py
@@ -0,0 +1,44 @@
+import os
+
+from ament_index_python import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, ExecuteProcess
+from launch_ros.actions import Node
+from launch.substitutions import LaunchConfiguration
+from launch_ros.descriptions import ComposableNode
+from ament_index_python import get_package_share_directory
+
+
+
+def generate_launch_description():
+
+    nav2_yaml = os.path.join(get_package_share_directory)
+
+    # Nodes
+    map_server = Node(
+        package='nav2_map_server',
+        executable='map_server',
+        name='map_server',
+        output='screen',
+        parameters=[{'use_sim_time':True},
+                    {'yaml_filename': 'map/Masterlabben.yaml'}
+                    ]
+    )
+    amcl = Node(
+        package='nav2_amcl',
+        executable='amcl',
+        name='amcl',
+        output='screen',
+        parameters=[{'yaml_filename':'src/tb3_cpp/params/amcl_params.yaml'}]
+    )
+
+
+    # Launch Description
+    ld = LaunchDescription()
+
+
+    # Nodes
+    ld.add_action(map_server)
+    ld.add_action(amcl)
+
+    return ld
--- tb_ws/src/tb3_cpp/launch/nav2_gazebo_launch.py
+++ tb_ws/src/tb3_cpp/launch/nav2_gazebo_launch.py
@@ -8,36 +8,66 @@ from launch_ros.actions import Node
 from launch_ros.actions import SetRemap
 from launch.actions import GroupAction
 from launch_ros.actions import PushRosNamespace
-
-os.environ["TURTLEBOT3_MODEL"] = "waffle"
+import time
 
 print('hei')
 
+os.environ["TURTLEBOT3_MODEL"] = "burger"
+
 def generate_launch_description():
 
-    return LaunchDescription([
-
-        GroupAction(
-            actions=[
-                #SetRemap(src='/odom',dst='follower/odom'),
-                #SetRemap(src='/cmd_vel',dst='follower/cmd_vel'),
-                #SetRemap(src='/scan',dst='follower/scan'),
-                PushRosNamespace('tb'),
+    tb_waffle= GroupAction(
+        actions=[
+            # ros2 launch turtlebot3_gazebo turtlebot3_world.launch.py 
+            IncludeLaunchDescription(
                 
-                # ros2 launch turtlebot3_gazebo turtlebot3_world.launch.py 
-                IncludeLaunchDescription(
-                    
-                    PythonLaunchDescriptionSource(
-                        os.path.join(
-                            get_package_share_directory('turtlebot3_gazebo'),
-                            'launch/turtlebot3_world.launch.py'
-                        )
-                    ),
-                    launch_arguments={
-                        'x_pose':'-2.0',
-                        'y_pose':'0.5' 
-                    }.items()
+                PythonLaunchDescriptionSource(
+                    os.path.join(
+                        get_package_share_directory('turtlebot3_gazebo'),
+                        'launch/turtlebot3_world.launch.py'
+                    )
                 ),
-            ]
-        )
-   ])
+                launch_arguments={
+                    #'x_pose':'-2.0',
+                    #'y_pose':'0.5' 
+                }.items()
+            ),
+        ]
+    )
+
+
+
+    tb_burger= GroupAction(
+        actions=[
+            #SetRemap(src='/odom',dst='follower/odom'),
+            #SetRemap(src='/cmd_vel',dst='follower/cmd_vel'),
+            #SetRemap(src='/scan',dst='follower/scan'),
+            PushRosNamespace('tb'),
+            
+            # ros2 launch turtlebot3_gazebo turtlebot3_world.launch.py 
+            IncludeLaunchDescription(
+                
+                PythonLaunchDescriptionSource(
+                    os.path.join(
+                        get_package_share_directory('turtlebot3_gazebo'),
+                        'launch/spawn_turtlebot3.launch.py'
+                    )
+                ),
+                launch_arguments={
+                    'x_pose':'-2.0',
+                    'y_pose':'0.5' 
+                }.items()
+            ),
+        ]
+    )
+
+    ld = LaunchDescription()
+    
+    #os.environ["TURTLEBOT3_MODEL"] = "waffle"
+    #ld.add_action(tb_waffle)
+    #time.sleep(20)
+
+
+    ld.add_action(tb_burger)
+
+    return ld
