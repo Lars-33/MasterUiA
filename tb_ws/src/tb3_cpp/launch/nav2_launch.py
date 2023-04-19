aca9aa198c5975321e286e0b00b31d6b1c7ecc3e
Lars-33 <larsmuggerud@proton.me>
2023-04-19 09:19:28 +0200

    aa

--- tb_ws/src/tb3_cpp/launch/nav2_launch.py
+++ tb_ws/src/tb3_cpp/launch/nav2_launch.py
@@ -5,13 +5,10 @@ from ament_index_python import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch_ros.actions import Node
-
 #from launch.substitutions import PathJoinSubstitution, LaunchConfiguration, Command, FindExecutable 
 from launch_ros.substitutions import FindPackageShare
-
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-
 from launch_ros.actions import SetRemap
 from launch.actions import GroupAction
 from launch_ros.actions import PushRosNamespace
