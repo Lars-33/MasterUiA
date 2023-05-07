24fe68c0aab14ff291289e1374670c8f9e17d827
Lars-33 <97943685+Lars-33@users.noreply.github.com>
2023-05-07 18:52:30 +0200

    Update tb3_launch.py

--- tb_ws/src/tb3_cpp/launch/tb3_launch.py
+++ tb_ws/src/tb3_cpp/launch/tb3_launch.py
@@ -1,31 +1,23 @@
 import os
-
 from ament_index_python import get_package_share_directory
-
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch_ros.actions import Node
-
 #from launch.substitutions import PathJoinSubstitution, LaunchConfiguration, Command, FindExecutable 
 from launch_ros.substitutions import FindPackageShare
-
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-
 from launch_ros.actions import SetRemap
 from launch.actions import GroupAction
 from launch_ros.actions import PushRosNamespace
 
-
 os.environ["TURTLEBOT3_MODEL"] = "waffle"
-
 os.system('sudo chmod a+rw /dev/ttyACM0')
 os.system('sudo chmod 666 /dev/ttyUSB0')
 
 def generate_launch_description():
 
     return LaunchDescription([
-
         #SetRemap(src='/odom',dst='/tb/odom'),
         #SetRemap(src='/cmd_vel',dst='/tb/cmd_vel'),
         #DeclareLaunchArgument("tb", default_value=TextSubstitution(text="tb")),
@@ -48,4 +40,3 @@ def generate_launch_description():
             ]
         )
     ])
-
